55e25786d8de599b643bf5c8ba865c96668c40a6
Cameron Noemdo <cameron@nebula.co.za>
2025-08-12 09:29:26 +0200

    Revert changes

--- ostentans-poe/src/main.tsx
+++ ostentans-poe/src/main.tsx
@@ -1,17 +1,10 @@
-import { StrictMode, useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { FluentProvider, webLightTheme, webDarkTheme } from '@fluentui/react-components';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import './index.css';
-
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-const [theme] = useState(prefersDark ? webDarkTheme : webLightTheme);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FluentProvider theme={theme}>
-      <App />
-    </FluentProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
