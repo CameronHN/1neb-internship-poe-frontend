65fc8fb18c2ac23bf1778b571a4e5a468b50390a
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-30 09:51:41 +0200

    Center elements in login page

--- ostentans-poe/src/pages/LoginPage.tsx
+++ ostentans-poe/src/pages/LoginPage.tsx
@@ -2,7 +2,16 @@ import LoginForm from "../components/auth/LoginForm";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => (
-  <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 16 }}>
+  <div
+    style={{
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      gap: 16,
+      justifyContent: "center",
+    }}
+  >
     <h1>Login</h1>
     <LoginForm />
     <Link to="/register">Don't have an account? Register</Link>
