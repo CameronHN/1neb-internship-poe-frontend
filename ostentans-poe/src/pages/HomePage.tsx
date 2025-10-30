386a2562b0010f6f91a1823e184092ead4f837c8
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-10-30 09:50:53 +0200

    Update to move login submit button to component file

--- ostentans-poe/src/pages/HomePage.tsx
+++ ostentans-poe/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import LoginForm from "../components/auth/LoginForm";
-import { Text } from "@fluentui/react-components";
+import { Text, Button } from "@fluentui/react-components";
+import { ArrowDown12Regular, ColorBackground20Filled } from "@fluentui/react-icons";
 import "../App.css";
 
 const HomePage = () => {
@@ -15,13 +16,33 @@ const HomePage = () => {
         justifyContent: "center",
       }}
     >
-      <Text as="h1" size={1000}>
+      <Text as="h1" size={1000} style={{ color: "black" }}>
         <span className="font-kapakana">O</span>
         <span className="font-katibeh">stentans</span>
       </Text>
+
       <LoginForm />
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 12,
+        }}
+      >
+        <Button
+          type="submit"
+          appearance="primary"
+          className="form-submit-button"
+          icon={<ArrowDown12Regular />}
+          style={{ alignSelf: "flex-start" }}
+        >
+          Register
+        </Button>
+
+        <Link to="/">Try a demo</Link>
+      </div>
     </div>
   );
 };
