15495756fbb86997995b782218fc74b63a18ef24
Cameron Noemdo <cameronnoemdo@outlook.com>
2025-11-16 20:03:45 +0200

    Add type for Certification

--- ostentans-poe/src/types/certificationTypes.ts
+++ ostentans-poe/src/types/certificationTypes.ts
@@ -1,4 +1,17 @@
+export interface AddCertification {
+    certificationName: string;
+    issuingOrganisation: string;
+    credentialUrl: string | null;
+    issuedDate: string | null;
+    expiryDate: string | null;
+}
+
+export interface UpdateCertification extends AddCertification {
+    id: string;
+}
+
 export interface Certification {
+    id: string;
     certificationName: string;
     issuingOrganisation: string;
     credentialUrl: string | null;
